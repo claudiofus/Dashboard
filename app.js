4a511f610386632b6025a82e036122834ef23454
Claudiofus <claudiofus@hotmail.it>
2018-03-08 21:49:24 +0100

    Redesign login

--- app.js
+++ app.js
@@ -11,6 +11,11 @@ app.use(express.static(__dirname + '/public'));
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/views/login.html');
 });
+
+app.get('/login', function (req, res) {
+    res.sendFile(__dirname + '/views/login.html');
+});
+
 app.get('/dashboard', function (req, res) {
     res.sendFile(__dirname + '/views/dashboard.html');
 });
@@ -24,10 +29,12 @@ app.get('/commissioni', function (req, res) {
     res.sendFile(__dirname + '/views/commissioni.html');
 });
 
+var id = '';
+
 io.on('connection', function (socket) {
     socket.on('tokenid', function (tokenId) {
         if (tokenId && tokenId !== '') {
-            socket.tokenId = tokenId;
+            id = tokenId;
             socket.emit('redirect', '/dashboard');
         }
     });
